6fd58d78517bcd5b8e9bfb4f0eaf76e8e46722cf
Alberto <alberto.estebanez.rodrigo@alumnos.upm.es>
2018-07-17 19:45:07 +0200

    archivos raspis use

--- r4_U.py
+++ r4_U.py
@@ -37,36 +37,32 @@ def to_1400MHz():
 
 
 while(True):
-	#Raspi      R2   R4   R6
-	#work 0:    X    O    X
-	#work 1:    O    O    X
-	#work 2:    O    O    O
-	#work 3:    X    O    O
+	#Raspi      R3   R1   R5    R6   R2   R4
+	#work 0:    X    O    X     X    O    X
+	#work 1:    X    O    X     X    O    O
+	#work 2:    X    O    X     O    O    X
+	#work 3:    X    O    X     O    O    O
+
+	#work 4:    X    O    O     X    O    X
+	#work 5:    X    O    O     X    O    O
+	#work 6:    X    O    O     O    O    X
+	#work 7:    X    O    O     O    O    O
+
+	#work 8:    O    O    X     X    O    X
+	#work 9:    O    O    X     X    O    O
+	#work 10:   O    O    X     O    O    X
+	#work 11:   O    O    X     O    O    O
+
+	#work 12:   O    O    O     X    O    X
+	#work 13:   O    O    O     X    O    O
+	#work 14:   O    O    O     O    O    X
+	#work 15:   O    O    O     O    O    O
 
 	#CPU to 600 MHz
 	to_600MHz()
 
 	#work 0 - 35 min
-	work_1()
-	time.sleep(330) 
-	kill_work_1()
-	time.sleep(90) 
-	work_2()
-	time.sleep(330) 
-	kill_work_2()
-	time.sleep(90) 
-	work_4()
-	time.sleep(330) 
-	kill_work_4()
-	time.sleep(90) 
-	work_2()
-	time.sleep(330) 
-	kill_work_2()
-	time.sleep(90) 
-	work_1()
-	time.sleep(330) 
-	kill_work_1()
-	time.sleep(90)
+	time.sleep(2100) 
 
 	#work 1 - 30 min
 	work_1()
@@ -91,26 +87,8 @@ while(True):
 	time.sleep(60)
 
 	#work 2  -  40 min
-	work_1()
-	time.sleep(360) 
-	kill_work_1()
-	time.sleep(120) 
-	work_2()
-	time.sleep(360) 
-	kill_work_2()
-	time.sleep(120) 
-	work_4()
-	time.sleep(360) 
-	kill_work_4()
-	time.sleep(120) 
-	work_2()
-	time.sleep(360) 
-	kill_work_2()
-	time.sleep(120) 
-	work_1()
-	time.sleep(360) 
-	kill_work_1()
-	time.sleep(120) 
+	time.sleep(2400) 
+
 
 	#work 3  -  35 min
 	work_1()
@@ -134,39 +112,11 @@ while(True):
 	kill_work_1()
 	time.sleep(90)
 
+	#work 4 - 35 min
+	time.sleep(2100) 
 
-	#Raspi      R2   R4   R6
-	#work 0:    O    O    X
-	#work 1:    X    O    O
-	#work 2:    X    O    X
-	#work 3:    O    O    O
 
-	#CPU to 1.4 GHz 
-	to_1400MHz()
-
-	#work 0  -  40 min
-	work_1()
-	time.sleep(360) 
-	kill_work_1()
-	time.sleep(120) 
-	work_2()
-	time.sleep(360) 
-	kill_work_2()
-	time.sleep(120) 
-	work_4()
-	time.sleep(360) 
-	kill_work_4()
-	time.sleep(120) 
-	work_2()
-	time.sleep(360) 
-	kill_work_2()
-	time.sleep(120) 
-	work_1()
-	time.sleep(360) 
-	kill_work_1()
-	time.sleep(120) 
-
-	#work 1  -  30 min
+	#work 5 - 30 min
 	work_1()
 	time.sleep(300) 
 	kill_work_1()
@@ -188,7 +138,10 @@ while(True):
 	kill_work_1()
 	time.sleep(60)
 
-	#work 2  -  35 min
+	#work 6  -  40 min
+	time.sleep(2400) 
+
+	#work 7  -  35 min
 	work_1()
 	time.sleep(330) 
 	kill_work_1()
@@ -209,6 +162,145 @@ while(True):
 	time.sleep(330) 
 	kill_work_1()
 	time.sleep(90)
+
+	#work 8 - 35 min
+	time.sleep(2100) 
+
+
+	#work 9 - 30 min
+	work_1()
+	time.sleep(300) 
+	kill_work_1()
+	time.sleep(60) 
+	work_2()
+	time.sleep(300) 
+	kill_work_2()
+	time.sleep(60) 
+	work_4()
+	time.sleep(300) 
+	kill_work_4()
+	time.sleep(60) 
+	work_2()
+	time.sleep(300) 
+	kill_work_2()
+	time.sleep(60) 
+	work_1()
+	time.sleep(300) 
+	kill_work_1()
+	time.sleep(60)
+
+	#work 10  -  40 min
+
+	time.sleep(2400) 
+
+
+	#work 11  -  35 min
+	work_1()
+	time.sleep(330) 
+	kill_work_1()
+	time.sleep(90) 
+	work_2()
+	time.sleep(330) 
+	kill_work_2()
+	time.sleep(90) 
+	work_4()
+	time.sleep(330) 
+	kill_work_4()
+	time.sleep(90) 
+	work_2()
+	time.sleep(330) 
+	kill_work_2()
+	time.sleep(90) 
+	work_1()
+	time.sleep(330) 
+	kill_work_1()
+	time.sleep(90)
+
+	#work 12 - 35 min
+	time.sleep(2100) 
+
+
+	#work 13 - 30 min
+	work_1()
+	time.sleep(300) 
+	kill_work_1()
+	time.sleep(60) 
+	work_2()
+	time.sleep(300) 
+	kill_work_2()
+	time.sleep(60) 
+	work_4()
+	time.sleep(300) 
+	kill_work_4()
+	time.sleep(60) 
+	work_2()
+	time.sleep(300) 
+	kill_work_2()
+	time.sleep(60) 
+	work_1()
+	time.sleep(300) 
+	kill_work_1()
+	time.sleep(60)
+
+	#work 14  -  40 min
+	time.sleep(2400) 
+
+
+	#work 15  -  35 min
+	work_1()
+	time.sleep(330) 
+	kill_work_1()
+	time.sleep(90) 
+	work_2()
+	time.sleep(330) 
+	kill_work_2()
+	time.sleep(90) 
+	work_4()
+	time.sleep(330) 
+	kill_work_4()
+	time.sleep(90) 
+	work_2()
+	time.sleep(330) 
+	kill_work_2()
+	time.sleep(90) 
+	work_1()
+	time.sleep(330) 
+	kill_work_1()
+	time.sleep(90)
+
+
+	#Raspi      R3   R1   R5    R6   R2   R4
+	#work 0:    X    O    X     X    O    X
+	#work 1:    O    O    X     O    O    X
+	#work 2:    O    O    O     O    O    X
+	#work 3:    X    O    X     O    O    O
+
+	#work 4:    X    O    O     X    O    X
+	#work 5:    O    O    X     X    O    O
+	#work 6:    O    O    O     X    O    O
+	#work 7:    X    O    O     O    O    O
+
+	#work 8:    O    O    X     X    O    X
+	#work 9:    X    O    O     X    O    O
+	#work 10:   X    O    X     X    O    O
+	#work 11:   O    O    X     O    O    O
+
+	#work 12:   O    O    O     X    O    X
+	#work 13:   X    O    O     O    O    X
+	#work 14:   X    O    X     O    O    X
+	#work 15:   O    O    O     O    O    O
+
+	#CPU to 1.4 GHz 
+	to_1400MHz()
+
+	#work 0  -  40 min
+	time.sleep(2400)  
+
+	#work 1  -  30 min
+	time.sleep(1800)
+
+	#work 2  -  35 min
+	time.sleep(2100)
 
 	#work 3  -  40 min
 	work_1()
@@ -231,3 +323,172 @@ while(True):
 	time.sleep(360) 
 	kill_work_1()
 	time.sleep(120) 
+
+	#work 4  -  40 min
+	time.sleep(2400) 
+
+	#work 5  -  30 min
+	work_1()
+	time.sleep(300) 
+	kill_work_1()
+	time.sleep(60) 
+	work_2()
+	time.sleep(300) 
+	kill_work_2()
+	time.sleep(60) 
+	work_4()
+	time.sleep(300) 
+	kill_work_4()
+	time.sleep(60) 
+	work_2()
+	time.sleep(300) 
+	kill_work_2()
+	time.sleep(60) 
+	work_1()
+	time.sleep(300) 
+	kill_work_1()
+	time.sleep(60)
+
+	#work 6  -  35 min
+	work_1()
+	time.sleep(330) 
+	kill_work_1()
+	time.sleep(90) 
+	work_2()
+	time.sleep(330) 
+	kill_work_2()
+	time.sleep(90) 
+	work_4()
+	time.sleep(330) 
+	kill_work_4()
+	time.sleep(90) 
+	work_2()
+	time.sleep(330) 
+	kill_work_2()
+	time.sleep(90) 
+	work_1()
+	time.sleep(330) 
+	kill_work_1()
+	time.sleep(90)
+
+	#work 7  -  40 min
+	work_1()
+	time.sleep(360) 
+	kill_work_1()
+	time.sleep(120) 
+	work_2()
+	time.sleep(360) 
+	kill_work_2()
+	time.sleep(120) 
+	work_4()
+	time.sleep(360) 
+	kill_work_4()
+	time.sleep(120) 
+	work_2()
+	time.sleep(360) 
+	kill_work_2()
+	time.sleep(120) 
+	work_1()
+	time.sleep(360) 
+	kill_work_1()
+	time.sleep(120) 
+
+	#work 8  -  40 min
+	time.sleep(2400) 
+
+	#work 9  -  30 min
+	work_1()
+	time.sleep(300) 
+	kill_work_1()
+	time.sleep(60) 
+	work_2()
+	time.sleep(300) 
+	kill_work_2()
+	time.sleep(60) 
+	work_4()
+	time.sleep(300) 
+	kill_work_4()
+	time.sleep(60) 
+	work_2()
+	time.sleep(300) 
+	kill_work_2()
+	time.sleep(60) 
+	work_1()
+	time.sleep(300) 
+	kill_work_1()
+	time.sleep(60)
+
+	#work 10  -  35 min
+	work_1()
+	time.sleep(330) 
+	kill_work_1()
+	time.sleep(90) 
+	work_2()
+	time.sleep(330) 
+	kill_work_2()
+	time.sleep(90) 
+	work_4()
+	time.sleep(330) 
+	kill_work_4()
+	time.sleep(90) 
+	work_2()
+	time.sleep(330) 
+	kill_work_2()
+	time.sleep(90) 
+	work_1()
+	time.sleep(330) 
+	kill_work_1()
+	time.sleep(90)
+
+	#work 11  -  40 min
+	work_1()
+	time.sleep(360) 
+	kill_work_1()
+	time.sleep(120) 
+	work_2()
+	time.sleep(360) 
+	kill_work_2()
+	time.sleep(120) 
+	work_4()
+	time.sleep(360) 
+	kill_work_4()
+	time.sleep(120) 
+	work_2()
+	time.sleep(360) 
+	kill_work_2()
+	time.sleep(120) 
+	work_1()
+	time.sleep(360) 
+	kill_work_1()
+	time.sleep(120) 
+
+	#work 12  -  40 min
+	time.sleep(2400) 
+
+	#work 13  -  30 min
+	time.sleep(1800)
+
+	#work 14  -  35 min
+	time.sleep(2100)
+
+	#work 15  -  40 min
+	work_1()
+	time.sleep(360) 
+	kill_work_1()
+	time.sleep(120) 
+	work_2()
+	time.sleep(360) 
+	kill_work_2()
+	time.sleep(120) 
+	work_4()
+	time.sleep(360) 
+	kill_work_4()
+	time.sleep(120) 
+	work_2()
+	time.sleep(360) 
+	kill_work_2()
+	time.sleep(120) 
+	work_1()
+	time.sleep(360) 
+	kill_work_1()
+	time.sleep(120)
